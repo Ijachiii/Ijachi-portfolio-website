4e448d083c4c43f4a685ee0ae3fe084521af3810
CYQIKS_03 <davidaudu1010@gmail.com>
2022-10-14 11:11:16 +0100

    second commit

--- website/basicapp/forms.py
+++ website/basicapp/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Contact
 
+
 class ContactForm(forms.ModelForm):
 
     class Meta():
@@ -8,8 +9,8 @@ class ContactForm(forms.ModelForm):
         fields = ("name", "email", "phone_number", "message")
 
         widgets = {
-        "name": forms.TextInput(attrs={"class": "form-name form-control", "placeholder": "Name*", "style": "width: 500px; height:50px; background-color: #343A40; color: #E6E6E6"}),
-        "email": forms.EmailInput(attrs={"class": "form-email form-control", "placeholder": "Email*", "style": "width: 500px; height:50px; background-color: #343A40; color: #E6E6E6"}),
-        "phone_number": forms.TextInput(attrs={"class": "form-name form-control", "placeholder": "Phone Number*", "style": "width: 500px; height:50px; background-color: #343A40; color: #E6E6E6"}),
-        "message": forms.Textarea(attrs={"class": "form-message form-control", "placeholder": "Message*", "style": "width: 500px; background-color: #343A40; color: #E6E6E6"})
+            "name": forms.TextInput(attrs={"class": "form-name form-control", "placeholder": "Name*", "style": "width: 500px; height:50px; background-color: #343A40; color: #E6E6E6"}),
+            "email": forms.EmailInput(attrs={"class": "form-email form-control", "placeholder": "Email*", "style": "width: 500px; height:50px; background-color: #343A40; color: #E6E6E6"}),
+            "phone_number": forms.TextInput(attrs={"class": "form-name form-control", "placeholder": "Phone Number*", "style": "width: 500px; height:50px; background-color: #343A40; color: #E6E6E6"}),
+            "message": forms.Textarea(attrs={"class": "form-message form-control", "placeholder": "Message*", "style": "width: 500px; background-color: #343A40; color: #E6E6E6"})
         }
